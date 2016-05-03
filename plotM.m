f54c296b0050739b9ca70f557e6210b438eba3d9
mrisso <matheush.risso@gmail.com>
2016-05-03 11:54:08 -0300

    Tabelas prontas

--- plotM.m
+++ plotM.m
@@ -23,12 +23,19 @@ function plotM (f,a,x0,y0,p,xf)
 	eDP = errors(a,x,yDP);
 
 	%Tabelas
+	fprintf('%59s\n\n','==Resultados==');
+	fprintf('%15s | %15s | %15s | %15s | %15s | %15s | %15s\n', 'x', 'Analitica', 'Euler', 'Euler Melhorado', 'Euler Modificado', 'Runge-Kutta', 'Dormand-Prince');
+	for i = 1: 128 fprintf('='); end; fprintf('\n');
 	for i = 1: length(x)
-		fprintf('%10.3f  10.3%f  10.3%f  10.3%f  10.3%f  10.3%f  10.3%f\n', x(i), y(i), yM(i), yMod(i), yR(i), yDP(i));
+		fprintf('%15.2f | %15.2f | %15.2f | %15.2f | %15.2f | %15.2f | %15.2f\n', x(i), a(x(i)), y(i), yM(i), yMod(i), yR(i), yDP(i));
 	end
 
+	fprintf('\n\n\n');
+	fprintf('%59s\n\n','==Erros==');
+	fprintf('%15s | %15s | %15s | %15s | %15s | %15s\n', 'x', 'Euler', 'Euler Melhorado', 'Euler Modificado', 'Runge-Kutta', 'Dormand-Prince');
+	for i = 1: 128 fprintf('='); end; fprintf('\n');
 	for i = 1: length(x)
-		fprintf('10.3%f  10.3%f  10.3%f  10.3%f  10.3%f  10.3%f\n', x(i), eE(i), eM(i), eMod(i), eR(i), eDP(i));
+		fprintf('%15.2f | %15.2f | %15.2f | %15.2f | %15.2f | %15.2f\n', x(i), eE(i), eM(i), eMod(i), eR(i), eDP(i));
 	end
 
 end
